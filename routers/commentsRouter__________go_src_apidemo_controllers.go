6fb32dff8977ac88d1f3e173b07d34b2a0960e46
Weidaicheng <wdcdavyc@outlook.com>
2019-03-16 15:20:22 +0800

    getcity

---
+++ routers/commentsRouter__________go_src_apidemo_controllers.go
@@ -0,0 +1,127 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["apidemo/controllers:ObjectController"] = append(beego.GlobalControllerRouter["apidemo/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:ObjectController"] = append(beego.GlobalControllerRouter["apidemo/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:ObjectController"] = append(beego.GlobalControllerRouter["apidemo/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/:objectId`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:ObjectController"] = append(beego.GlobalControllerRouter["apidemo/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:objectId`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:ObjectController"] = append(beego.GlobalControllerRouter["apidemo/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:objectId`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:ObjectController"] = append(beego.GlobalControllerRouter["apidemo/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "GetCity",
+            Router: `/GetCity`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:UserController"] = append(beego.GlobalControllerRouter["apidemo/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:UserController"] = append(beego.GlobalControllerRouter["apidemo/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:UserController"] = append(beego.GlobalControllerRouter["apidemo/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/:uid`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:UserController"] = append(beego.GlobalControllerRouter["apidemo/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:uid`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:UserController"] = append(beego.GlobalControllerRouter["apidemo/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:uid`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:UserController"] = append(beego.GlobalControllerRouter["apidemo/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Login",
+            Router: `/login`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["apidemo/controllers:UserController"] = append(beego.GlobalControllerRouter["apidemo/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Logout",
+            Router: `/logout`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
